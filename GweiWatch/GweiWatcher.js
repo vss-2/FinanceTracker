7a20f17f22fa61f3907fa7397b525f7d13a9eb74
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2022-01-03 15:51:55 -0300

    Update GweiWatcher.js
    Feature download file with price and timestamp

--- GweiWatch/GweiWatcher.js
+++ GweiWatch/GweiWatcher.js
@@ -16,13 +16,25 @@ function exportarValores(){
 	let output = [];
 	for(let v = 0; v < valores.length; v++){
 		output.push({
-			v: new Object({
+			[v]: new Object({
 				"price": valores[v], 
 				"timestamp": timestamps[v] 
 			})
 		});
 	}
 	console.log(JSON.stringify(output));
+}
+
+// Download arquivo
+function salvar(){
+	var valoresBlob = new Blob([exportarValores()], {type: 'application/json'});
+	var aTag  = document.createElement('a');
+	aTag.href = URL.createObjectURL(valoresBlob);
+	aTag.download  = "valores.json";
+	aTag.hidden    = true;
+	document.body.appendChild(aTag);
+	aTag.innerHTML = "Salvar";
+	aTag.click();
 }
 
 // Observer feito para monitorar http://polygonscan.com
