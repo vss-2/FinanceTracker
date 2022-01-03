74d3d1a404ee85e86eb4e04d012f8f9403706d80
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2022-01-03 15:25:32 -0300

    Create GweiWatcher.js

---
+++ GweiWatch/GweiWatcher.js
@@ -0,0 +1,32 @@
+// Monitora valores
+const valorMaximo = 100;
+let valores    = [];
+let timestamps = [];
+const observer = new MutationObserver(
+    function(mut){
+        valores.push(mut[0].addedNodes[0].textContent.split(' ')[0]);
+        timestamps.push(new Date());
+        if(valores[valores.length-1] < valorMaximo){ 
+            new Notification('Entrar no mercado');
+        } 
+    }
+);
+
+function exportarValores(){
+	let output = [];
+	for(let v = 0; v < valores.length; v++){
+		output.push({
+			v: new Object({
+				"price": valores[v], 
+				"timestamp": timestamps[v] 
+			})
+		});
+	}
+	JSON.stringify(output);
+}
+
+// Observer feito para monitorar http://polygonscan.com
+observer.observe(
+    document.getElementById('fastgas'), 
+    { characterData: true, attributes: true, childList: true } 
+);
